9cdfe205d304b5eda01a5361abbf46ca76f9185e
Nimra050304 <arminpcemail1@gmail.com>
2025-04-13 23:25:02 +0530

    Update reminder_pf_2nums.c

--- pps/chap1/reminder_pf_2nums.c
+++ pps/chap1/reminder_pf_2nums.c
@@ -6,7 +6,7 @@ int main(){
     printf("\nInput the second number\n");
     scanf("%f",&b);
     if(b!=0){
-        printf("\nThe reminder when %f is divided by %f is\n",a%b);}
+        printf("\nThe reminder when %f is divided by %f is %f\n",a,b,a%b);}
     else{
         printf("\nThe division is not possible\n");
     return 0;
