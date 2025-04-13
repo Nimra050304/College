0a3881c8e378ac052d82d283aa5d7831ba95cc2b
Nimra050304 <arminpcemail1@gmail.com>
2025-04-13 23:25:34 +0530

    Update and rename product_of_2nums to product_of_2nums.c

--- pps/chap1/product_of_2nums.c
+++ pps/chap1/product_of_2nums.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%f",&a);
     printf("\nInput the second number\n");
     scanf("%f",&b);
-    printf("\nThe product of the two numbers is %f\n",a*b);
+    printf("\nThe product of %f and %f is %f\n",a,b,a*b);
 
 
     return 0;
