c967cf6dcc8bf0e56b3796dc0feab56afdad771f
Nimra050304 <arminpcemail1@gmail.com>
2025-04-13 23:18:44 +0530

    Create reminder_pf_2nums.c

---
+++ pps/chap1/reminder_pf_2nums.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    float a,b;
+    printf("\nInput the first number\n");
+    scanf("%f",&a);
+    printf("\nInput the second number\n");
+    scanf("%f",&b);
+    if(b!=0)
+
+    return 0;
+}
