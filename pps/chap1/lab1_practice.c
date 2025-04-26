bb32d5a0672095da29a955465c41bf6b9c5e15b0
Nimra050304 <arminpcemail1@gmail.com>
2025-04-26 20:50:42 +0530

    Create lab1_practice.c

---
+++ pps/chap1/lab1_practice.c
@@ -0,0 +1,15 @@
+/*In this code, we are using operators, specifically bitwise operators. For this lab we were supposed to use all opertaors but 
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    int a=32,b=34;
+    printf("a&b is equal to %d\n",a&b );
+    printf("a|b is equal to %d\n", a|b);
+    printf("a^b is equal to %d \n", a^b);
+    printf("~a is equal to %d\n", ~a);//gives us 2's compliment
+
+    return 0;
+}
